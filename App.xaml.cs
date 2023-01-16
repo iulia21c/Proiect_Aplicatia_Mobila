584190b8dcd64489510cf2f06308b190725c9e52
Crisan Iulia <crisaniuliaa21@gmail.com>
2023-01-16 22:50:41 +0200

    Magazin

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,26 @@
-﻿namespace Proiect_Aplicatia_Mobila;
+﻿using System;
+using System.IO;
+using Proiect_Aplicatia_Mobila.Data;
+
+namespace Proiect_Aplicatia_Mobila;
 
 public partial class App : Application
 {
+    static ShoppingListDatabase database;
+    public static ShoppingListDatabase Database
+    {
+        get
+        {
+            if (database == null)
+            {
+                database = new
+               ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+               LocalApplicationData), "ShoppingList.db3"));
+            }
+            return database;
+        }
+    }
+
 	public App()
 	{
 		InitializeComponent();
